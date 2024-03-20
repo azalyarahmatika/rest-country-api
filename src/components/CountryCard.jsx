d20315662da9853213c3f5cfcc2b2f92aa0e86ea
Azalya Rahmatika <azalyarahmatika@gmail.com>
2024-03-20 22:08:07 +0300

    accessibility fixing

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -2,9 +2,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function CountryCard({name, flags, population, region, capital}) {
+  const encodedName = encodeURIComponent(name);
   return (
     <div className="card">
-      <Link to={`/country/${name}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+      <Link to={`/country/${encodedName}`} style={{ textDecoration: 'none', color: 'inherit' }}>
         <img src={flags} alt={name} />
         <div className="card_info">
           <h2>{name}</h2>
--- src/components/FilterInput.jsx
+++ src/components/FilterInput.jsx
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 function FilterInput({ regions, onRegionChange }) {
   function onChange(event) {
     onRegionChange(event.target.value);
-    console.log(event.target.value)
   }
 
   return (
-    <select className="filter_input" id="language" onChange={onChange} >
+    <>
+      <label htmlFor="region"></label>
+      <select className="filter_input" id="region" onChange={onChange} >
       <option value="">Filter By Region</option>
       {regions.map((region, index) => (
         <option key={index} value={region}>{region}</option>
       ))}
     </select>
+    </>
   );
 }
 
